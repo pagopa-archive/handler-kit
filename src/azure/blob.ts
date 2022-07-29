bda981be29cf4833f3ec7da55ef1955fae684748
Luca Cavallaro <luca.cavallaro@pagopa.it>
2022-07-29 06:29:57 +0200

    feat!: support queue and blob storage trigger
    Reviewed-By: @gunzip @grausof 
    Refs: SFEQS-910

---
+++ src/azure/blob.ts
@@ -0,0 +1,53 @@
+import * as azure from "@azure/functions";
+
+import * as t from "io-ts";
+
+import * as E from "fp-ts/Either";
+import * as O from "fp-ts/Option";
+import { pipe } from "fp-ts/function";
+
+import { sequenceS } from "fp-ts/lib/Apply";
+import { validate } from "../validation";
+import { trigger, InvalidTriggerError } from "./trigger";
+
+const isBlobTriggeredFunctionContext = (
+  ctx: azure.Context
+): ctx is azure.Context & {
+  bindingData: {
+    uri: string;
+    metadata: unknown;
+  };
+} =>
+  pipe(
+    trigger(ctx.bindingDefinitions),
+    O.filter((t) => t.type === "blobTrigger"),
+    O.isSome
+  );
+
+export type Blob<T> = {
+  uri: string;
+  metadata: T;
+};
+
+export const fromBlobStorage =
+  <T>(metadataSchema: t.Decoder<unknown, T>) =>
+  (ctx: azure.Context): E.Either<Error, Blob<T>> =>
+    pipe(
+      ctx,
+      E.fromPredicate(
+        isBlobTriggeredFunctionContext,
+        () =>
+          new InvalidTriggerError(
+            "This function can be triggered only by Blob Storage"
+          )
+      ),
+      E.chainW((ctx) =>
+        sequenceS(E.Apply)({
+          metadata: validate(
+            metadataSchema,
+            "Unable to validate the metadata schema"
+          )(ctx.bindingData.metadata),
+          uri: E.right(ctx.bindingData.uri),
+        })
+      )
+    );
---
+++ src/azure/http.ts
@@ -0,0 +1,33 @@
+import * as azure from "@azure/functions";
+
+import * as O from "fp-ts/Option";
+import * as E from "fp-ts/Either";
+import { pipe } from "fp-ts/function";
+
+import { HttpRequest } from "../http/request";
+import { validate } from "../validation";
+
+import { trigger, InvalidTriggerError } from "./trigger";
+
+const isHttpTriggeredFunctionContext = (
+  ctx: azure.Context
+): ctx is azure.Context & { req: azure.HttpRequest } =>
+  pipe(
+    trigger(ctx.bindingDefinitions),
+    O.filter((t) => t.type === "httpTrigger"),
+    O.isSome
+  );
+
+export const fromHttpRequest = (ctx: azure.Context) =>
+  pipe(
+    ctx,
+    E.fromPredicate(
+      isHttpTriggeredFunctionContext,
+      () =>
+        new InvalidTriggerError(
+          "This function can be triggered only by an HTTP request"
+        )
+    ),
+    E.map((ctx) => ctx.req),
+    E.chainW(validate(HttpRequest, "Unable to validate the Azure HTTP request"))
+  );
