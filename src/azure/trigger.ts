eb029a8fb6ed3b931e547e0df92f633dc514c0c8
grausof <grausof@gmail.com>
2023-03-02 12:22:46 +0100

    Add eventHub handler

--- src/azure/trigger.ts
+++ src/azure/trigger.ts
@@ -3,14 +3,16 @@ import * as azure from "@azure/functions";
 import { findFirst } from "fp-ts/Array";
 
 export type AzureFunctionTrigger = {
-  type: "queueTrigger" | "httpTrigger" | "blobTrigger";
+  type: "queueTrigger" | "httpTrigger" | "blobTrigger" | "eventHubTrigger";
   direction: "in";
   name: string;
 };
 
 export const trigger = findFirst(
   (b: azure.BindingDefinition): b is AzureFunctionTrigger =>
-    ["queueTrigger", "httpTrigger", "blobTrigger"].includes(b.type)
+    ["queueTrigger", "httpTrigger", "blobTrigger", "eventHubTrigger"].includes(
+      b.type
+    )
 );
 
 export class InvalidTriggerError extends Error {
